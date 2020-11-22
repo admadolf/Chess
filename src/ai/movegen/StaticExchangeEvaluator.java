9af7e1bcbcd7ec2bda356881b52b1e8e1abad50e
adam.adolf <adm.adolf@gmail.com>
2020-11-22 19:59:51 +0100

    New playground, SEE

---
+++ src/ai/movegen/StaticExchangeEvaluator.java
@@ -0,0 +1,17 @@
+package ai.movegen;
+
+import ai.representation.Board;
+
+/**
+ * This class focuses on calculating the exchanges that can happen on a given square
+ * @author konek
+ *
+ */
+public class StaticExchangeEvaluator {
+
+	
+	public void evaluate(Board position, int on) {
+		
+	}
+	
+}
